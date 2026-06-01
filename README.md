67adcae5d1dddc1fe4f6c1893b3504d60098904b
bogaledemasrepo <bogidemas@gmail.com>
2026-06-01 07:42:57 +0000

    Readme file update to include folder structure

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # FastCommerce
+
+## Folder structure
+
+    FASTCOMMERCE/
+    ├── dist/
+    ├── node_modules/
+    ├── public/
+    ├── src/
+    │   ├── assets/
+    │   ├── components/
+    │   ├── context/
+    │   ├── pages/
+    │   ├── index.css
+    │   └── main.tsx
+    ├── .gitignore
+    ├── bun.lock
+    ├── eslint.config.js
+    ├── index.html
+    ├── package.json
+    ├── README.md
+    ├── tsconfig.app.json
+    ├── tsconfig.json
+    ├── tsconfig.node.json
+    ├── vercel.json
+    └── vite.config.ts
--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -4,13 +4,18 @@ import { useCart } from "../context/cart-context";
 function ProductCard({ item }: { item: number }) {
   const { addToCart } = useCart();
   return (
+    // <div className="card bg-base-100 max-w-96 shadow-sm">
+
+    // </div>
     <Link
       to={"/detail"}
-      className="border border-gray-300 rounded-md h-72 grow min-w-45 max-w-[320px] flex flex-col relative"
+      className="border card border-gray-300/50 hover:border-blue-500 duration-200 rounded-md h-78 grow min-w-45 max-w-[320px] flex flex-col relative"
     >
-      <div className="p-4">ProductCard {item}</div>
+      <div className="p-2 w-40 h-40">
+        <image href="../assets/hero.png" className="bg-red-500" width={200} height={200}/>
+      </div>
 
-      <button
+      {/* <button
         onClick={(e) => {
           e.preventDefault();
           addToCart("Boo");
@@ -18,7 +23,7 @@ function ProductCard({ item }: { item: number }) {
         className="p-2 border border-blue-300 rounded-md absolute left-2 right-2 bottom-2 flex grow"
       >
         Add to cart
-      </button>
+      </button> */}
     </Link>
   );
 }
