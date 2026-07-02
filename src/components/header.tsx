7395c721c3711758f3d7968721dec7a7110b42e1
bogaledemasrepo <bogidemas@gmail.com>
2026-07-02 12:15:59 +0300

    updated hard coded avator

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -33,7 +33,7 @@ function Header() {
               <div className="w-10 rounded-full">
                 <img
                   alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                  src={user.avatar} />
               </div>
             </div>
             <ul
